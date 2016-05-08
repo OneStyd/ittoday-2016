0b7962a9d6cfed810cc53bff024fa9f456e82586
OneStyd <wawansetyadi33@gmail.com>
2016-05-08 13:46:03 +0700

    Perbaikan Form

--- ittoday/page/login.php
+++ ittoday/page/login.php
@@ -1,5 +1,4 @@
 <?php
-	//session_start();
 	if(isset($_SESSION['ittoday_user'])){
 		header("location: ./");
 	}
@@ -57,120 +56,114 @@
 		mysqli_free_result($cek);
 		mysqli_close($conn);
 	}
-
-
 ?>	
-
-	<section id="intro" class="intro-section page-section">
-		<div class="row-login" style="text-align:left;">
-			<div class="col-md-5 col-md-offset-1">
-				<h3>Login</h3>
-				<form id="login" action="" method="post">
-					<input type="hidden" name="login" value="1"/>
-					<div class="form-group">
-						<label for="email">Email</label><br/>
-						<input type="email" class="form-control" name="email" id="email" required/>
-					</div>
-					<div class="form-group">
-						<label for="password">Password</label><br/>
-						<input type="password" class="form-control" name="password" id="password" required/>
-					</div>
-					<button type="submit" class="btn btn-danger" value="Proceed">Proceed</button>
-				</form>
-			</div>
-			<div class="col-md-5">
-				<script>
-					function cekPass(){
-                        var pass = document.getElementById('password_r');
+		<section id="intro" class="intro-section page-section">
+			<div class="row-login" style="text-align:left;">
+				<div class="col-md-5 col-md-offset-1">
+					<h2>Login</h2>
+					<form id="login" action="" method="post">
+						<input type="hidden" name="login" value="1"/>
+						<div class="form-group">
+							<label for="email">Email</label><br/>
+							<input type="email" class="form-control" name="email" id="email" required/>
+						</div>
+						<div class="form-group">
+							<label for="password">Password</label><br/>
+							<input type="password" class="form-control" name="password" id="password" required/>
+						</div>
+						<button type="submit" class="btn btn-danger" value="Proceed">Proceed</button><br><br>
+					</form>
+				</div>
+				<div class="col-md-5">
+					<script>
+						function cekPass(){
+							var pass = document.getElementById('password_r');
 						
-						if(pass1.value == ""){
-							$('#pass_error2').hide();
-						}else{
-							$('#pass_error2').show();
+							if(pass1.value == ""){
+								$('#pass_error2').hide();
+							}
+							else{
+								$('#pass_error2').show();
+							}
 						}
 
-                    }
+						function valid(){
+							validasiEmail();
+							disabledT();
+						}
 
-                    function valid(){
-                    	validasiEmail();
-                    	disabledT();
-                    }
-
-                    function validasiEmail(){
-                    	var email = $("#email_r").val();
+						function validasiEmail(){
+							var email = $("#email_r").val();
                     	
-                    	if(email == ""){
-                    		$("#email_error").hide();
-                    	}else{
-                    		$("#email_error").show();
-                    	}
+							if(email == ""){
+								$("#email_error").hide();
+							}
+							else{
+								$("#email_error").show();
+							}
 
-                    	jQuery.ajax({
-                             url: "./author/validasi.php",
-                             data:{email: email},
-                             async: false,  
-                             type: "POST",
-                             success:function(data){
-                                 $("#email_error").html(data);
-                                 //$("#loaderIcon").hide();
-                             },
-                             error:function (){}
-                             });
+							jQuery.ajax({
+								url: "./author/validasi.php",
+								data:{email: email},
+								async: false,  
+								type: "POST",
+								success:function(data){
+									$("#email_error").html(data);
+								},
+								error:function (){}
+							});
                   
-               		 }
-
-               		 
-
-					function cekPass2(){
-                        var pass1 = document.getElementById('password_r');
-						var pass2 = document.getElementById('password2_r');
-
-
-						if(pass1.value == pass2.value){
-							document.getElementById('pass_error2').innerHTML = "Password Match";
-						}
-						else{
-							document.getElementById('pass_error2').innerHTML = "Password Doesn't Match";
 						}
 
-						if(pass2.value == ""){
-							$('#pass_error2').hide();
-						}else{
-							$('#pass_error2').show();
-						}
+						function cekPass2(){
+							var pass1 = document.getElementById('password_r');
+							var pass2 = document.getElementById('password2_r');
 
-                    }
-				</script>
-				<h3>Registrasi</h3>
-				<form id="registrasi" action="" method="post">
-					<input type="hidden" name="registrasi" value="1"/>
-					<div class="form-group">
-						<label for="email_r">Email*</label><br/>
-						<input type="email" class="form-control" name="email" onkeyup="valid()" id="email_r" required/>
-						<span id="email_error"></span>
-					</div>
-					<script type="text/javascript">
-					function disabledT(){
-               		 	if($("#email_error").html() != ""){
-               		 		$("#registrasi_btn").attr("disabled",false);
-               		 	}else{
-               		 		$("#registrasi_btn").attr("disabled",true);
-               		 	}
-               		 }
-               		</script>
-					<div class="form-group">
-						<label for="password_r">Password*</label><br/>
-						<input type="password" class="form-control" name="password" id="password_r" required/>
-					</div>
-					<div class="form-group">
-						<label for="password2_r">Konfirmasi Password*</label><br/>
-						<input type="password" class="form-control" name="password2" id="password2_r" onkeyup="cekPass2()" required/>
-						<div id='pass_error2' class='error'></div>
-					</div>
-					<input type="checkbox" id="field_terms" name="setuju" value="1" required> Saya bersedia mematuhi aturan main IT Today 2016<br/><br/>
-					<button type="submit" class="btn btn-danger" value="Proceed" id="registrasi_btn" >Proceed</button>
-				</form>
+							if(pass1.value == pass2.value){
+								document.getElementById('pass_error2').innerHTML = "Password Match";
+							}
+							else{
+								document.getElementById('pass_error2').innerHTML = "Password Doesn't Match";
+							}
+
+							if(pass2.value == ""){
+								$('#pass_error2').hide();
+							}
+							else{
+								$('#pass_error2').show();
+							}
+						}
+					</script>
+					<h2>Registrasi</h2>
+					<form id="registrasi" action="" method="post">
+						<input type="hidden" name="registrasi" value="1"/>
+						<div class="form-group">
+							<label for="email_r">Email<span>*</span></label><br/>
+							<input type="email" class="form-control" name="email" onkeyup="valid()" id="email_r" required/>
+							<span id="email_error"></span>
+						</div>
+						<script type="text/javascript">
+							function disabledT(){
+								if($("#email_error").html() != ""){
+									$("#registrasi_btn").attr("disabled",false);
+								}
+								else{
+									$("#registrasi_btn").attr("disabled",true);
+								}
+							}
+						</script>
+						<div class="form-group">
+							<label for="password_r">Password<span>*</span></label><br/>
+							<input type="password" class="form-control" name="password" id="password_r" required/>
+						</div>
+						<div class="form-group">
+							<label for="password2_r">Konfirmasi Password<span>*</span></label><br/>
+							<input type="password" class="form-control" name="password2" id="password2_r" onkeyup="cekPass2()" required/>
+							<div id='pass_error2' class='error'></div>
+						</div>
+						<input type="checkbox" id="field_terms" name="setuju" value="1" required> Saya bersedia mematuhi aturan main IT Today 2016<br/><br/>
+						<button type="submit" class="btn btn-danger" value="Proceed" id="registrasi_btn" >Proceed</button><br><br>
+					</form>
+				</div>
 			</div>
-		</div>
-    </section>
-
+		</section>
--- ittoday/page/user.php
+++ ittoday/page/user.php
@@ -2,19 +2,16 @@
 	$user = mysqli_query($conn, "SELECT * FROM ittoday WHERE email = '".$_SESSION['ittoday_user']."'");
 	$info = mysqli_fetch_assoc($user);
 ?>    
-
-
-
-    <section id="intro" class="intro-section page-section">
-        <!-- <div class="container"> -->
+		<section id="intro" class="intro-section page-section">
             <div class="container" style="text-align:left">
                 <div class="col-md-5 col-md-offset-1">
-                	<?php if($info['nama_lengkap']==NULL && $info['no_hp']==NULL && $info['alamat']==NULL
-                				&& $info['no_identitas']==NULL && $info['institusi']==NULL && $info['tahun_masuk']==NULL){
-                		$disabled = "disabled";
+                	<?php 
+						if($info['nama_lengkap']==NULL && $info['no_hp']==NULL && $info['alamat']==NULL && 
+						$info['no_identitas']==NULL && $info['institusi']==NULL && $info['tahun_masuk']==NULL){
+						$disabled = "disabled";
                 	?>
-                	<h3>Mohon Lengkapi Informasi Anda Terlebih Dahulu</h3>
-                		<form id="registrasi2" action="" method="post">
+                	<h2>Mohon Lengkapi Informasi Anda Terlebih Dahulu</h2>
+                	<form id="registrasi2" action="" method="post">
 						<input type="hidden" name="registrasi" value="1"/>
 						<div class="form-group">
 							<label for="username">Nama Lengkap</label><br/>
@@ -40,56 +37,43 @@
 							<label for="tahun_masuk">Tahun Masuk</label><br/>
 							<input type="number" class="form-control" name="tahun_masuk" id="tahun_masuk" required/>
 						</div>
-						<button type="submit" class="btn btn-danger" value="Proceed">Proceed</button>
-                	<?php }else{ $disabled = ""; ?>
-                		<h3>Informasi Pendaftar</h3>
-                		<div class="col-md-12">               
-						    <div class="row">
-						        <div class="col-md-4">Nama</div>
-						        <div class="col-md-8"><?php echo $info['nama_lengkap'] ?></div>
-						    </div>
-						    <div class="row">
-						        <div class="col-md-4">No. Hape</div>
-						        <div class="col-md-8"><?php echo $info['no_hp'] ?></div>
-						    </div>
-						    <div class="row">
-						        <div class="col-md-4">Identitas</div>
-						        <div class="col-md-8"><?php echo $info['no_identitas'] ?></div>
-						    </div>
-						    <div class="row">
-						        <div class="col-md-4">Institusi</div>
-						        <div class="col-md-8"><?php echo $info['institusi'] ?></div>
-						    </div>
-						    <div class="row">
-						        <div class="col-md-4">Tahun Masuk</div>
-						        <div class="col-md-8"><?php echo $info['tahun_masuk'] ?></div>
-						    </div>
-						    <div class="row">
-						        <div class="col-md-4">Alamat</div>
-						        <div class="col-md-8"><?php echo $info['alamat'] ?></div>
-						    </div>
-						</div>
-
-                	<?php } ?>
-
+						<button type="submit" class="btn btn-danger" value="Proceed">Proceed</button><br><br>
+					</form>
+                	<?php 
+						} else{ 
+							$disabled = ""; 
+					?>
+                	<h2>Informasi Pendaftar</h2>        
+					<table class="table">	
+						<tr><td>Nama</td><td><?php echo $info['nama_lengkap'] ?></td></tr>
+						<tr><td>No. HP</td><td><?php echo $info['no_hp'] ?></td></tr>
+						<tr><td>Identitas</td><td><?php echo $info['no_identitas'] ?></td></tr>
+						<tr><td>Institusi</td><td><?php echo $info['institusi'] ?></td></tr>
+						<tr><td>Tahun Masuk</td><td><?php echo $info['tahun_masuk'] ?></td></tr>
+						<tr><td>Alamat</td><td><?php echo $info['alamat'] ?></td></tr>
+					</table>
+                	<?php 
+						} 
+					?>
 				</div>
-				<div class="col-md-5">
-					<h3>PILIHAN ACARA</h3>
+				<div class="col-md-5 col-md-offset-1">
+					<h2>PILIHAN ACARA</h2>
 					<form id="pilihan_acara" method="post" >
-						<input type="hidden" name="pilcar" value="<?php echo $info['id_user']; ?>"/> <!-- tampilkan id pendaftar -->
+						<input type="hidden" name="pilcar" value="<?php echo $info['id_user']; ?>"/>
 						<div class="form-group">
 							<?php if($disabled=="disabled") { ?>
-							<label for="isi_dulu">Sebelum mendaftarkan diri pada salah satu acara, silakan lengakpi informasi diri Anda terlebih dahulu</label><br/>
+								<label for="isi_dulu">
+									Sebelum mendaftarkan diri pada salah satu acara, silakan lengakpi informasi diri Anda terlebih dahulu
+								</label>
 							<?php } ?>
-							<label for="pilcar">Anda boleh memilih lebih dari 1 acara:</label><br/>
-							<sub>Jika masih bingung, Anda boleh langsung melewati tahapan ini.</sub><br/><br/>
-							<label><input type="checkbox" name="agricode" id="agricode" <?php echo $disabled ?>> Agricode [Competitive Prgramming] <br/></label>
+							<label for="pilcar">Anda boleh memilih lebih dari 1 acara:</label>
+							<h6>Jika masih bingung, Anda boleh langsung melewati tahapan ini</h6><br>
+							<label><input type="checkbox" name="agricode" id="agricode" <?php echo $disabled ?>> Agricode [Competitive Prgramming]</label><br/>
 							<script type="text/javascript">
 								$('#agricode').click(function() {
-								$('#agricode-set')[this.checked ? "show" : "hide"]();
-							});
+									$('#agricode-set')[this.checked ? "show" : "hide"]();
+								});
 							</script>
-							<div class=""><div class="">
 							<fieldset id="agricode-set" hidden>
 									<label for='ukuran_baju_ac'>Ukuran Baju</label><br/>
 			                        <select id="ukuran_baju_ac" name="ukuran_baju_ac" class="form-control">
@@ -98,20 +82,18 @@
 			                            <option>L</option>
 			                            <option>XL</option>
 			                            <option>XXL</option>
-			                        </select>
-									<br/><label class="control-label">Upload Identitas (Kartu Mahasiswa/KTP)<br/>
-									<sub>Maksimal 1MB, format: .jpg atau .png</sub><br/><br/>
+			                        </select><br>
+									<label class="control-label">Upload Identitas (Kartu Mahasiswa/KTP)
+									<h6>Maksimal 1 MB, format: .jpg atau .png</h6><br>
 									<input id="ktm_agricode" type="file" name="ktm_ac" class="file">					
 									<br/>
 							</fieldset>
-							</div></div>
-							<label><input type="checkbox" name="agrihack" id="agrihack" <?php echo $disabled ?>> Agrihack [Capture The Flag]<br/></label>
+							<label><input type="checkbox" name="agrihack" id="agrihack" <?php echo $disabled ?>> Agrihack [Capture The Flag]</label><br/>
 							<script type="text/javascript">
 								$('#agrihack').click(function() {
-								$('#agrihack-set')[this.checked ? "show" : "hide"]();
-							});
+									$('#agrihack-set')[this.checked ? "show" : "hide"]();
+								});
 							</script>
-							<div class=""><div class="">
 							<fieldset id="agrihack-set" hidden>
 									<label for='ukuran_baju_ac'>Ukuran Baju</label><br/>
 			                        <select id="ukuran_baju_ah" name="ukuran_baju_ah" class="form-control">
@@ -120,48 +102,39 @@
 			                            <option>L</option>
 			                            <option>XL</option>
 			                            <option>XXL</option>
-			                        </select>
-									<br/><label class="control-label">Upload Identitas (Kartu Mahasiswa/KTP)<br/>
-									<sub>Maksimal 1MB, format: .jpg atau .png</sub><br/><br/>
-									<input id="ktm_agrihack" type="file" name="ktm_ah" class="file">					
-									<br/>
+			                        </select><br>
+									<label class="control-label">Upload Identitas (Kartu Mahasiswa/KTP)
+									<h6>Maksimal 1 MB, format: .jpg atau .png</h6><br>
+									<input id="ktm_agrihack" type="file" name="ktm_ah" class="file"><br>
 							</fieldset>
-							</div></div>
-							<label><input type="checkbox" name="igdc" id="igdc" <?php echo $disabled ?>> IPB Game Dev Competition<br/></label>
+							<label><input type="checkbox" name="igdc" id="igdc" <?php echo $disabled ?>> IPB Game Dev Competition</label><br/>
 							<script type="text/javascript">
 								$('#igdc').click(function() {
-								$('#igdc-set')[this.checked ? "show" : "hide"]();
-							});
+									$('#igdc-set')[this.checked ? "show" : "hide"]();
+								});
 							</script>
-							<div class=""><div class="">
 							<fieldset id="igdc-set" hidden> 
 								<label for="teamname">Nama Tim</label><br/>
 								<input type="text" class="form-control" name="nama_tim" id="nama_tim"/>
 								<label for="teammate1">Nama Anggota 1</label><br/>
-								<input type="text" class="form-control" name="anggota_1" id="anggota_1" />
+								<input type="text" class="form-control" name="anggota_1" id="anggota_1"/>
 								<label for="teammate1_id">No Identitas Anggota 1</label><br/>
-								<input type="text" class="form-control" name="id_anggota_1" id="id_anggota_1" />
+								<input type="text" class="form-control" name="id_anggota_1" id="id_anggota_1"/>
 								<label for="teammate2">Nama Anggota 2</label><br/>
-								<input type="text" class="form-control" name="anggota_2" id="anggota_2" />
+								<input type="text" class="form-control" name="anggota_2" id="anggota_2"/>
 								<label for="teammate2_id">No Identitas Anggota 1</label><br/>
-								<input type="text" class="form-control" name="id_anggota_2" id="id_anggota_2" />
-								<br/><label class="control-label">Upload Identitas Tim (Kartu Mahasiswa/KTP)<br/>
-									<sub>Maksimal 1MB, format: jadikan satu dalam bentuk PDF</sub><br/><br/>
-									<input id="ktm_agrihack" type="file" name="ktm_ah" class="file">
-								<br/>
+								<input type="text" class="form-control" name="id_anggota_2" id="id_anggota_2"/><br>
+								<label class="control-label">Upload Identitas Tim (Kartu Mahasiswa/KTP)<br/>
+								<h6>Maksimal 1MB, format: jadikan satu dalam bentuk PDF</h6><br/>
+								<input id="ktm_agrihack" type="file" name="ktm_ah" class="file"><br>
 							</fieldset>
-							</div></div>
 							<label><input type="checkbox" name="isc" id="isc" <?php echo $disabled ?>> IPB Searching Competition</label><br/>
-							<label><input type="checkbox" name="seminar" id="seminar" <?php echo $disabled ?>> Seminar IT</label><br/>
-							<br/>
-							<button type="submit" class="btn btn-danger" value="Proceed" <?php echo $disabled ?>>Proceed</button>
-							<!-- <button type="submit" class="btn btn-danger" name="skip" value="1">Lewati</button> -->
-
+							<label><input type="checkbox" name="seminar" id="seminar" <?php echo $disabled ?>> Seminar IT</label><br/><br/>
+							<button type="submit" class="btn btn-danger" value="Proceed" <?php echo $disabled ?>>Proceed</button><br><br>
 						</div>
 					</form>
 				</div>
             </div>
-        <!-- </div> -->
-    </section>
+		</section>
 
 
