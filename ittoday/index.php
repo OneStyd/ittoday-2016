96f74eab39bebc6a78d7bdfa9f1d055755933919
OneStyd <wawansetyadi33@gmail.com>
2016-05-08 12:08:53 +0700

    Perapihan dan Responsive Main

--- ittoday/index.php
+++ ittoday/index.php
@@ -6,334 +6,122 @@
 
 <!DOCTYPE html>
 <html lang="en">
+	<head>
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Website IT Today 2016">
+		<meta name="author" content="Tim Website IT Today 2016">
+		
+		<!-- Title -->
+		<title>IT Today IPB 2016 | Grow Indonesia's Future with Technology</title>
+		<link rel="icon" sizes="192x192" href="img/main_icon.png">
+		
+		<!-- jQuery -->
+		<script src="js/jquery.js"></script>
 
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>IT Today IPB 2016 | Grow Indonesia's Future with Technology</title>
-
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome/css/font-awesome.css" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link href="css/scrolling-nav.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/animate.min.css">
-    <style>
-        @font-face{
-            font-family: Arvo;
-            src: url(fonts/Arvo-Bold.ttf);
-            font-weight: bold;
-        }
-         @font-face{
-            font-family: Prototype;
-            src: url(fonts/Prototype.ttf);
-        }
-         @font-face{
-            font-family: RobotoSlab;
-            src: url(fonts/RobotoSlab-Regular.ttf);
-        }
-        @font-face{
-            font-family: Aller;
-            src: url(fonts/Aller_Rg.ttf);
-            font-weight: 500;
-        }
-        @font-face{
-            font-family: amicable;
-            src: url(fonts/perfectly_amicable.ttf);
-            font-weight: 500;
-        }
-        @font-face{
-            font-family: Comfortaa;
-            src: url(fonts/Comfortaa-Regular.ttf);
-            font-weight: 500;
-        }
-        @import url(//fonts.googleapis.com/css?family=Comfortaa);
-
-        .navbar{
-            background: #000;
-        }
-
-        .navbar-brand{
-            font-family: amicable;
-            font-size: 35px;
-        }
-
-        .navbar-default .navbar-brand{
-            color:#fff !important;
-        }
-
-        .navbar-default .navbar-nav>li>a, .navbar-default .navbar-nav>.active>a:focus, .navbar-default .navbar-nav>.active>a{
-            color: #fff !important;
-        }
-
-        .navbar-nav>li>a:hover{
-            color:#000 !important;
-            background: #fff !important;
-            border-radius: 10px;
-        }
-
-        .navbar-default .navbar-nav>.active>a, .navbar-default .navbar-nav>.active>a:focus, .navbar-default .navbar-nav>.active>a:hover{
-            color: #000 !important;
-            background: #fff !important;
-            border-radius: 10px;
-        }
-
-        .main-text{
-            padding-top:20%; 
-            font-family:amicable;
-            font-size:5vmax;
-            line-height:1;
-        }
-
-        @media(max-width:990px){
-            .main-icon>img{
-                /*width:50%;*/
-            }
-            .main-text{
-                padding-top: 0;
-            }
-        }
-
-        @media(min-width:1500px){
-            .main-icon>img{
-                margin: auto;
-                position: absolute;
-                top: 820px; left: 0; bottom: 0; right: 0;
-            }
-        }
-
-        .error{
-            color:blue;
-            font-style: italic;
-        }
-
-        #isc_slide{
-            position: fixed;
-            left: 10%;
-            right:10%;
-            bottom: 10%;
-            z-index: -1;
-        }
-
-        div.text > p, div.text > h2{
-            color: #fff;
-        }
-
-        .row-login {
-              position:absolute;
-              color:#000;
-              top:50%;
-              left:50%;
-              padding:15px;
-              -ms-transform: translateX(-50%) translateY(-50%);
-              -webkit-transform: translate(-50%,-50%);
-              transform: translate(-50%,-50%);
-              width: 100%;
-        }
-
-        .main-icon>img{
-            max-width:100%;
-            height: auto;
-
-        }
-
-        .section-icon>img{
-            max-width: 70%;
-            height: auto;
-            margin-top: 5px;
-            margin-bottom: 10px;
-        }
-
-        /*.col-md-6, .col-md-12, div.col-md-6>h3, .row{
-            font-size: 1vmax !important;
-        }*/
-        .row>.col-md-6>.main-icon>img{
-            padding-bottom: 5%;
-        }
-
-        div.col-lg-12>p, div.col-lg-12>ul, div.col-lg-12>li{
-            font-size: 2vmax;
-            margin-right: 10%;
-            margin-left: 10%;
-            z-index: 5;
-        }
-
-        h1{
-            font-size:4vmax;
-        }
-
-        .btn-ittoday{
-            background-color: #800c24;
-            color:#fff;
-            position: relative;
-            left:30%;
-        }
-       
-        html, body{
-            font-family: Comfortaa, Aller;
-            color:#800c24;
-        }
-
-    </style>
+		<!-- Bootstrap Core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/font-awesome.min.css" rel="stylesheet">
+		
+		<!-- Custom CSS -->
+		<link href="css/scrolling-nav.css" rel="stylesheet">
+		<link href="css/animate.min.css" rel="stylesheet">
+		<link href="css/custom.css" rel="stylesheet">
     
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
 
-<!-- The #page-top ID is part of the scrolling feature - the data-spy and data-target are part of the built-in Bootstrap scrollspy function -->
-
-<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top top-nav-collapse" role="navigation">
-        <div class="container-fluid">
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <?php if(empty($_GET['page'])){ ?>
-                    <a class="navbar-brand page-scroll" href="#page-top">IT Today<!-- <img src="img/logo_putih.png"> --></a>
-                <?php }else if(!empty($_GET['page'])){ ?>
-                    <a class="navbar-brand page-scroll" href="./">IT Today<!-- <img src="img/logo_putih.png"> --></a>
-                <?php } ?>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav">
-                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
-                    <?php if(empty($_GET['page'])){ ?>
-
-                        <li class="hidden">
-                            <a class="page-scroll" href="#page-top"></a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="#igdc-main">IGDC</a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="#isc-main">ISC</a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="#agricode-main">Agricode</a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="#agrihack-main">Agrihack</a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="#seminar-main">Seminar IT</a>
-                        </li>
-                    <?php }else if(!empty($_GET['page'])){ ?>
-                        <li class="hidden">
-                            <a class="page-scroll" href="./#page-top"></a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="./#igdc-main">IGDC</a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="./#isc-main">ISC</a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="./#agricode-main">Agricode</a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="./#agrihack-main">Agrihack</a>
-                        </li>
-                        <li>
-                            <a class="page-scroll" href="./#seminar-main">Seminar IT</a>
-                        </li>
-                    <?php } ?>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                  <?php if(!cek_login()){ ?>
-                        <li><a href="login"><span class="glyphicon glyphicon-user"></span>Login/Register</a></li>
+	<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+		<!-- Navigation -->
+		<nav class="navbar navbar-inverse navbar-fixed-top top-nav-collapse" role="navigation">
+			<div class="container-fluid">
+				<!-- Logo & Toggle Menu -->
+				<div class="navbar-header page-scroll">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+					</button>
+					<?php if(empty($_GET['page'])){ ?>
+						<a class="navbar-brand page-scroll" href="#page-top"><img class="navimg" src="img/logo_putih.png"></a>
+					<?php }else if(!empty($_GET['page'])){ ?>
+						<a class="navbar-brand page-scroll" href="./"><img class="navimg" src="img/logo_putih.png"></a>
+					<?php } ?>
+				</div>
+				
+				<!-- Nav Menu -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+						<?php if(empty($_GET['page'])){ ?>
+							<li class="hidden"><a class="page-scroll" href="#page-top"></a></li>
+							<li><a class="page-scroll" href="#igdc-main">IGDC</a></li>
+							<li><a class="page-scroll" href="#isc-main">ISC</a></li>
+							<li><a class="page-scroll" href="#agricode-main">Agricode</a></li>
+							<li><a class="page-scroll" href="#agrihack-main">Agrihack</a></li>
+							<li><a class="page-scroll" href="#seminar-main">Seminar IT</a></li>
+						<?php }else if(!empty($_GET['page'])){ ?>
+							<li class="hidden"><a class="page-scroll" href="#page-top"></a></li>
+							<li><a class="page-scroll" href="./#igdc-main">IGDC</a></li>
+							<li><a class="page-scroll" href="./#isc-main">ISC</a></li>
+							<li><a class="page-scroll" href="./#agricode-main">Agricode</a></li>
+							<li><a class="page-scroll" href="./#agrihack-main">Agrihack</a></li>
+							<li><a class="page-scroll" href="./#seminar-main">Seminar IT</a></li>
+						<?php } ?>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<?php if(!cek_login()){ ?>
+							<li><a href="login"><span class="glyphicon glyphicon-user"></span> Login/Register</a></li>
                         <?php }else{ ?>
-                        <li><a href="user"><span class="glyphicon glyphicon-user"></span>Profil</a></li>
-                        <li><a href="logout"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
-                    <?php } ?>
-                 </ul>
+							<li><a href="user"><span class="glyphicon glyphicon-user"></span> Profil</a></li>
+							<li><a href="logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+						<?php } ?>
+					</ul>
+				</div>
+			</div>
+		</nav>
 
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+		<!-- Page Section -->
+		<?php
+			if(empty($_GET['page'])){
+				include "page/main.php";
+			}
+			else if(isset($_GET['page'])){
+				$page = $_GET['page'];
+				if(include "page/".$page.".php");
+				else{
+					header("location: 404");
+				}
+			}
+		?>
 
-    <!-- Page -->
-    <?php
-        if(empty($_GET['page'])){
-            include "page/main.php";
+		<!-- jQuery -->
+		<script src="js/jquery.js"></script>
 
-        }else if(isset($_GET['page'])){
+		<!-- Bootstrap Core JavaScript -->
+		<script src="js/bootstrap.min.js"></script>
 
-            $page = $_GET['page'];
-
-            if(include "page/".$page.".php");
-            else{
-                header("location: 404");
-            }
-        }
-    ?>
-    <!-- End Page -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Scrolling Nav JavaScript -->
-    <script src="js/jquery.easing.min.js"></script>
-    <script src="js/scrolling-nav.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function() {
-        
-            function up() {
-                $("#bawah").animate({bottom: "-=30"}, 1500, "swing", down);
-            }
-            function down() {
-                $("#bawah").animate({bottom: "+=30"}, 1500, "swing", up);
-            }
-            down();
-    });
-    </script>
-    <script type="text/javascript">
-        // var isc = $('#isc-main').offset().top - window.innerHeight/2;
-        // var next = $('#igdc-main').offset().top - (window.innerHeight/2)+10;
-        //    $(window).scroll(function(){
-        //         var pTop = $('#page-top').scrollTop();
-        //         console.log( pTop + ' - ' + isc );
-        //         console.log( pTop + ' - ' + next );
-        //         if( pTop > isc && pTop < next ){
-        //             $('#isc-main').css({'background': 'url(img/isc/isc_nyala.jpg) center top no-repeat fixed', 'background-size': 'cover'});
-        //             $('#isc_slide').css('z-index', '0');
-        //             $('#isc_slide').show(1000);
-        //         }else{
-        //             $('#isc-main').css({'background': 'url(img/isc/isc_mati.jpg) center top no-repeat fixed', 'background-size': 'cover'});
-        //             $('#isc_slide').css('z-index', '-1');
-        //             $('#isc_slide').hide();
-        //             }
-        // });
-
-
-    </script>
-
-     <script src="js/jquery-sectionsnap.js"></script>
-
-
-</body>
-
+		<!-- Scrolling Nav JavaScript -->
+		<script src="js/jquery.easing.min.js"></script>
+		<script src="js/scrolling-nav.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				function up() {
+					$("#bawah").animate({bottom: "-=30"}, 1500, "swing", down);
+				}
+				function down() {
+					$("#bawah").animate({bottom: "+=30"}, 1500, "swing", up);
+				}
+				down();
+			});
+		</script>
+		
+		<!-- Section JS -->
+		<script src="js/jquery-sectionsnap.js"></script>
+	</body>
 </html>
--- ittoday/page/main.php
+++ ittoday/page/main.php
@@ -1,148 +1,112 @@
-<!-- Intro Section -->
-    <section id="intro" class="intro-section">
-        <div class="container">
-            <div class="row">
-                <!-- <div class="col-lg-12" style="border-radius:15px;"> -->
-                    <!-- <h1>Scrolling Nav</h1> -->
-                   <!--  <p><strong>Usage Instructions:</strong> Make sure to include the <code>scrolling-nav.js</code>, <code>jquery.easing.min.js</code>, and <code>scrolling-nav.css</code> files. To make a link smooth scroll to another section on the page, give the link the <code>.page-scroll</code> class and set the link target to a corresponding ID on the page.</p>
-                    <a class="btn btn-default page-scroll" href="#about">Click Me to Scroll Down!</a> -->
-                    <!-- <div class="row" style="height:100%;"> -->
+		<!-- Intro Section -->
+		<section id="intro" class="intro-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6" style="float: right">
+						<div class="main-text"><img src="img/logo.png"><p>#GrowIT</p></div>
+					</div>
+					<div class="col-md-6" style="float: left">
+						<div class="main-icon"><img src="img/burung.png"></div>
+					</div>
+				</div>
+			</div>
+			<div class="down-arrow" id="bawah">
+				<a href="#igdc-main" class="page-scroll"><i class="fa fa-angle-double-down fa-4x" ></i></a>
+			</div>    
+		</section>
 
-                        <div class="col-md-6">
-                           <div class="main-icon"><img src="img/burung.png"></div>
-                        </div>
-                        <div class="col-md-6">
-                            <!-- <div style="float:none;display:table-cell;vertical-align:bottom;position:absolute;height:100%"> -->
-                                <div class="main-text"><img src="img/logo.png" style="max-width:85%;height:auto">#GrowIT</div>
-                            <!-- </div> -->
-                        </div>
-                    <!-- </div> -->
-                     
-                                                        
-                <!-- </div> -->
-            </div>
-            <div class="down-arrow" id="bawah">
-            <a href="#igdc-main" class="page-scroll"><i class="fa fa-angle-double-down fa-4x" ></i></a> </div>    
-        </div>
-    </section>
+		<!-- IGDC Section -->
+		<section id="igdc-main" class="igdc-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<h1>IPB Game Developing Competition</h1>
+						<div class="section-icon"><img src="img/igdc/2.png"/></div>
+						<p>
+							Kegiatan berupa kompetisi antar-mahasiswa se-Jawa Barat dalam pembuatan game. 
+							Peserta akan diseleksi menjadi 10 tim terbaik untuk dapat mempresentasikan game di babak final. 
+							Seluruh tim pendaftar akan diundang ke acara final untuk menyaksikan para finalis IPB Game Dev Competition. 
+						</p>
+						<button type="button" class="btn btn-ittoday">DETAIL</button>
+					</div>
+				</div>
+			</div>
+		</section>
 
-    <section class="transition-section">
+		<!-- ISC Section -->
+		<section id="isc-main" class="isc-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<h1>IPB Searching Competition</h1>
+						<div class="section-icon"><img src="img/isc/3.png"/></div>
+						<p>
+							Kegiatan berupa kompetisi mencari informasi secara cepat dengan memanfaatkan fitur dari search engine. 
+							Pendaftaran dibuka untuk 100 peserta. Kegiatan ini bertemakan "Raising Your Searching Skill for Better Knowledge".
+						</p>
+						<button type="button" class="btn btn-ittoday">DETAIL</button>
+					</div>
+				</div>
+			</div>
+		</section>
 
-    </section>
+		<!-- Agricode Section -->
+		<section id="agricode-main" class="agricode-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<h1>Agricode - Competitive Programming</h1>
+						<div class="section-icon"><img src="img/agricode/4.png"/></div>
+						<p>Kegiatan berupa kompetisi pemrograman untuk siswa/i SMA sederajat dan diploma di seluruh indonesia.</p>
+						<button type="button" class="btn btn-ittoday">DETAIL</button>
+					</div>
+				</div>
+			</div>
+		</section>
 
-    <!-- IGDC Section -->
-    <section id="igdc-main" class="igdc-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1>IPB Game Developing Competition</h1>
-                    <div class="section-icon"><img src="img/igdc/2.png"/></div>
-                    <p>Kegiatan berupa kompetisi antar-mahasiswa se-Jawa Barat dalam pembuatan game. 
-                    Peserta akan diseleksi menjadi 10 tim terbaik untuk dapat mempresentasikan game di babak final. 
-                    Seluruh tim pendaftar akan diundang ke acara final untuk menyaksikan para finalis IPB Game Dev Competition. </p>
-                    <button type="button" class="btn btn-ittoday btn-danger">DETAIL</button>
-                </div>
-            </div>
-        </div>
-    </section>
+		<!-- Agrihack Section -->
+		<section id="agrihack-main" class="agricode-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<h1>Agrihack - Capture The Flag</h1>
+						<div class="section-icon"><img src="img/agrihack/5.png"/></div>
+						<p>Kegiatan berupa kompetisi dibidang cyber security untuk SMA/sederajat dan Diploma di seluruh indonesia</p>
+						<button type="button" class="btn btn-ittoday">DETAIL</button>
+					</div>
+				</div>
+			</div>
+		</section>
 
-    <section class="transition-section">
+		<!-- Seminar Section -->
+		<section id="seminar-main" class="seminar-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<h1>Seminar Nasional</h1>
+						<div class="section-icon"><img src="img/seminar/6.png"/></div>
+						<p>
+							Kegiatan puncak dari IT Today 2015 yang terdiri dari Seminar dan Showcase IGDC. 
+							Seminar bertemakan "The Future of Technology Devices".
+						</p>
+						<p>
+							Showcase IGDC dapat dihadiri oleh mahasiswa se-IPB, mahasiswa seluruh Indonesia, dan masyarakat sekitar kampus IPB 
+							gratis tanpa pendaftaran.
+						</p>
+						<button type="button" class="btn btn-ittoday">DETAIL</button>
+					</div>
+				</div>
+			</div>
+		</section>
 
-    </section>
-
-
-    <!-- ISC Section -->
-    <section id="isc-main" class="isc-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1>IPB Searching Competition</h1>
-                    <div class="section-icon"><img src="img/isc/3.png"/></div>
-                    <p>Kegiatan berupa kompetisi mencari informasi secara cepat dengan  
-                    memanfaatkan fitur dari search engine. Pendaftaran dibuka untuk 100  
-                    peserta. Kegiatan ini bertemakan  "Raising Your Searching Skill for Better Knowledge". </p>
-                    <button type="button" class="btn btn-ittoday btn-danger">DETAIL</button>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="transition-section">
-
-    </section>
-
-    <!-- Agricode Section -->
-    <section id="agricode-main" class="agricode-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1>Agricode - Competitive Programming</h1>
-                    <div class="section-icon"><img src="img/agricode/4.png"/></div>
-                    <p>Kegiatan berupa kompetisi pemrograman untuk siswa/i SMA sederajat 
-                    dan diploma di seluruh indonesia. </p>
-                    <button type="button" class="btn btn-ittoday btn-danger">DETAIL</button>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="transition-section">
-
-    </section>
-
-    <!-- Agrihack Section -->
-    <section id="agrihack-main" class="agricode-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1>Agrihack - Capture The Flag</h1>
-                    <div class="section-icon"><img src="img/agrihack/5.png"/></div>
-                    <p>Kegiatan berupa kompetisi dibidang cyber security   
-                    untuk SMA/sederajat dan Diploma di seluruh indonesia</p>
-                    <button type="button" class="btn btn-ittoday btn-danger">DETAIL</button>
-                </div>
-            </div>
-        </div>
-    </section>
-
-
-    <section class="transition-section">
-
-    </section>
-
-    <!-- Seminar Section -->
-    <section id="seminar-main" class="seminar-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1>Seminar Nasional</h1>
-                    <div class="section-icon"><img src="img/seminar/6.png"/></div>
-                    <p>Kegiatan puncak dari IT Today 2015 yang terdiri dari Seminar dan Showcase IGDC. Seminar bertemakan "The Future of Technology Devices".
-                       </p><!--  Terdiri dari 3 sesi yaitu:<p>
-                        <ul class="">
-                            <li>Sesi pertama menghadirkan ... </li>
-                            <li>Sesi kedua menghadirkan CEO Tokopedia, dan CEO IGrow</li>
-                            <li>Sesi kedua menghadirkan ... </li>
-                        </ul> -->
-                       <p> Showcase IGDC dapat dihadiri oleh mahasiswa se-IPB, mahasiswa seluruh Indonesia, dan masyarakat
-                        sekitar kampus IPB gratis tanpa pendaftaran.
-                    </p>
-                    <button type="button" class="btn btn-ittoday btn-danger">DETAIL</button>
-                </div>
-            </div>
-        </div>
-    </section>
-
-
-    <!-- Tanah Section -->
-    <section id="tanah-main" class="tanah-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <img src="img/logo.png" style="max-width:50%;margin-bottom:20px;"/><br/>
-                    <p>Grow Indonesia's Future with Technology</p>
-                </div>
-            </div>
-        </div>
-    </section>
-
+		<!-- Tanah Section -->
+		<section id="tanah-main" class="tanah-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<img src="img/logo.png">
+						<p>Grow Indonesia's Future with Technology</p>
+					</div>
+				</div>
+			</div>
+		</section>
