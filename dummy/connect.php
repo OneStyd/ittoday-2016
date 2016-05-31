5954bff21f36cfe656dc23bc7f44fa1f69e5ed5c
OneStyd <wawansetyadi33@gmail.com>
2016-05-31 20:58:31 +0700

    hapus file tidak terpakai

--- dummy/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-	
-	$conn = new mysqli('localhost', 'root', '', 'ittoday_2016');
-	$tablename = "ittoday";
-
-?>
--- parallax_final/php/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$connect = new mysqli("localhost", "root", "1234", "ittoday_2016");
-	$tableuser = "ittoday";
-?>
